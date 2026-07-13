f7a91a750469a8af02fa04b035a09e5fb7769c06
darkcybot <kibetc862@gmail.com>
2026-07-13 13:52:12 +0300

    Update integer.py

--- integer.py
+++ integer.py
@@ -6,5 +6,3 @@ print("Addition:",a+b)
 print("Subtraction:",a-b)
 print("Multiplication:",a*b)
 print("Division:",a/b)      #true division gives a float
-print("Modulus:", a % b)    #remainder after division
-print("Floor division:", a//b) #division rounded down to a whole number
